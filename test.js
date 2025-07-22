ed7945e85618ffbf441a6e0c1fded16cd33199aa
Komal <144211225+KomalkN@users.noreply.github.com>
2025-07-22 18:33:51 +0530

    Update test.js
    Signed-off-by: Komal <144211225+KomalkN@users.noreply.github.com>

--- test.js
+++ test.js
@@ -1,20 +1,10 @@
 const http = require('http');
-const { execFile } = require('child_process');
+const { exec } = require('child_process');
 
 http.createServer(function (req, res) {
   const userInput = req.url.slice(1); // simulate user input from URL
-  const safeInput = userInput.match(/^[\w\-]+$/) ? userInput : null;
-  if (safeInput) {
-    execFile(safeInput, [], (error, stdout, stderr) => {
-      if (error) {
-        res.write(`Error: ${error.message}`);
-      } else {
-        res.write(stdout);
-      }
-      res.end();
-    });
-  } else {
-    res.write("Invalid input");
+  exec(userInput, (error, stdout, stderr) => {
+    res.write(stdout);
     res.end();
-  }
+  });
 }).listen(8080);
